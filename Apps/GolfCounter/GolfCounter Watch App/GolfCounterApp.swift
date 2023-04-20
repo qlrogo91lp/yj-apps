bbbab29baf193b18f168b41d8104be5693282e39
yjkim <qlrogo91lp@gmail.com>
2023-04-20 09:35:41 +0900

    Initial Commit

---
+++ Apps/GolfCounter/GolfCounter Watch App/GolfCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfCounterApp.swift
+//  GolfCounter Watch App
+//
+//  Created by 윤재 on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct GolfCounter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apps/GolfCounter/GolfCounter/GolfCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfCounterApp.swift
+//  GolfCounter
+//
+//  Created by 윤재 on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct GolfCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
