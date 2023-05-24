f679c3225736a9d6b740846d6c7c429fdbe1b2c6
yjkim <qlrogo91lp@gmail.com>
2023-05-24 10:11:39 +0900

    Initial Commit

---
+++ Apps/TennisCounter/TennisCounter Watch App/TennisCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisCounterApp.swift
+//  TennisCounter Watch App
+//
+//  Created by 윤재 on 2023/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TennisCounter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apps/TennisCounter/TennisCounter/TennisCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisCounterApp.swift
+//  TennisCounter
+//
+//  Created by 윤재 on 2023/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TennisCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
